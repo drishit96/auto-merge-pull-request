2d9c4cbb257b9514533f6da51c7e950c7e817293
Drishit Mitra <drishit.mitra@gep.com>
2024-01-25 18:09:58 +0530

    Update query selectors

--- content.js
+++ content.js
@@ -1,20 +1,22 @@
 window.setInterval(function () {
-  let mergeButton = document.querySelector("#partial-pull-merging > div.merge-pr.js-merge-pr.js-details-container.Details.is-merging > div > div > div > div.merge-message > div.select-menu.d-inline-block > div > button.btn-group-merge.border-right-0.rounded-left-1.btn.BtnGroup-item.js-details-target");
-  let squashAndMergeButton = document.querySelector("#partial-pull-merging > div.merge-pr.js-merge-pr.js-details-container.Details.is-squashing > div > div > div > div.merge-message > div.select-menu.d-inline-block > div > button.btn-group-squash.border-right-0.rounded-left-1.btn.BtnGroup-item.js-details-target");
+  let mergeButton = document.querySelector("#partial-pull-merging > div.merge-pr.js-merge-pr.js-details-container.Details.is-merging.is-updating-via-merge > div > div > div > div > div.merge-message > div > div > button.btn-group-merge.rounded-left-2.btn.btn-primary.BtnGroup-item.js-details-target.hx_create-pr-button");
+  let squashAndMergeButton = document.querySelector("#partial-pull-merging > div.merge-pr.js-merge-pr.js-details-container.Details.is-updating-via-merge.is-squashing > div > div > div > div > div.merge-message > div > div > button.btn-group-squash.rounded-left-1.btn.btn-primary.BtnGroup-item.js-details-target.hx_create-pr-button");
   let updateButton = document.querySelector("#partial-pull-merging > div.merge-pr.js-merge-pr.js-details-container.Details.is-merging > div > div > div > div:nth-child(3) > form > button");
-  let confirmMergeButton = document.querySelector("#partial-pull-merging > div.merge-pr.js-merge-pr.js-details-container.Details.is-merging.open.Details--on > form > div > div.commit-form-actions > div > div.BtnGroup.btn-group-merge > button");
-  let confirmSquashAndMerge = document.querySelector("#partial-pull-merging > div.merge-pr.js-merge-pr.js-details-container.Details.is-squashing.open.Details--on > form > div > div.commit-form-actions > div > div.BtnGroup.btn-group-squash > button");
+  let confirmMergeButton = document.querySelector("#partial-pull-merging > div.merge-pr.js-merge-pr.js-details-container.Details.is-merging.is-updating-via-merge.open.Details--on > div > form > div > div.commit-form-actions > div > div.BtnGroup.btn-group-merge > button");
+  let confirmSquashAndMerge = document.querySelector("#partial-pull-merging > div.merge-pr.js-merge-pr.js-details-container.Details.is-updating-via-merge.is-squashing > div > form > div > div.commit-form-actions > div > div.BtnGroup.btn-group-squash > button");
 
-  if (confirmMergeButton && !confirmMergeButton.disabled) {
-    confirmMergeButton.click();
-    chrome.runtime.sendMessage({ triggerNotification: true }, function () {});
-  } else if (confirmSquashAndMerge && !confirmSquashAndMerge.disabled) {
-    confirmSquashAndMerge.click();
-    chrome.runtime.sendMessage({ triggerNotification: true }, function () {});
-  } else if (mergeButton && !mergeButton.disabled) {
+  if (mergeButton && !mergeButton.disabled) {
     mergeButton.click();
+    if (confirmMergeButton && !confirmMergeButton.disabled) {
+      confirmMergeButton.click();
+      chrome.runtime.sendMessage({ triggerNotification: true }, function () {});
+    }
   } else if (squashAndMergeButton && !squashAndMergeButton.disabled) {
     squashAndMergeButton.click();
+    if (confirmSquashAndMerge && !confirmSquashAndMerge.disabled) {
+      confirmSquashAndMerge.click();
+      chrome.runtime.sendMessage({ triggerNotification: true }, function () {});
+    }
   } else if (updateButton && !updateButton.disabled) {
     updateButton.click();
   }
